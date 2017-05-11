bdce5b6402294e60df4e840b333597ab6a764025
Riccardo <riccardo@Riccardos-MacBook-Pro.local>
2017-05-11 07:52:32 +0100

    feat(players) add player list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import * as firebase from 'firebase';
 import twemoji from 'twemoji'; 
 
 import './App.css';
+import PlayerCard from './PlayerCard';
 
 export default class App extends Component {
 
@@ -11,7 +12,28 @@ export default class App extends Component {
     this.state = {
       teams:[],
       selected: 'null',
+      players:[],
+      uid:null
     };
+  }
+
+  componentWillMount(){
+    
+    if(!window.localStorage.getItem('uid')){
+      firebase.auth().signInAnonymously().catch((error)=> {
+        alert('problem signing in refresh to try again ')  
+      });
+    }
+
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        const {uid} = user;
+        window.localStorage.setItem('uid',uid)
+        this.setState(prevState => ({uid}));
+      }
+
+    });
   }
 
   componentDidMount(){
@@ -28,12 +50,6 @@ export default class App extends Component {
         
       this.setState(prevState => ({teams}));
     })
-
-  }
-  selectTeam(name){
-    this.setState(prevState => ({
-      selected: name
-    }));
   }
 
   render() {
@@ -58,7 +74,37 @@ export default class App extends Component {
             })}
           </ul>
         </div>
+        <div className="players-list">
+          { 
+            this.state.players.map((player,i)=>{
+              return (<PlayerCard key={player.key} team={team} player={player} uid={this.state.uid}/>);
+            })
+          }
+        </div>
       </div>
     );
+  }
+
+  selectTeam(name){
+    this.getPlayers(name);
+    this.setState(prevState => ({
+      selected: name
+    }));
+  }
+
+  getPlayers(name){
+    let players = [];
+    const playerRef = firebase.database().ref('players').orderByChild('team').equalTo(name);
+    playerRef.on('value',snap =>{
+      const playerObj = snap.val();
+      if(playerObj){
+         players =  Object.keys(playerObj)
+          .map(key =>{
+            return Object.assign({key,},playerObj[key])
+          })
+      }
+      
+      this.setState(prevState => ({players}));
+    })
   }
 }
---
+++ src/PlayerCard.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+import twemoji from 'twemoji'; 
+import './PlayerCard.css';
+import * as firebase from 'firebase';
+
+export default class PlayerCard extends Component {
+    emojis = [
+        {id:'poo',value:'💩'},
+        {id:'thumbsDown',value:'👎'},
+        {id:'thinking',value:'🤔'},
+        {id:'ok',value:'👌'},
+        {id:'starPlayer',value:'🌟'},
+        {id:'worldClass',value:'🌎'}
+    ];
+
+    missingImage = "https://platform-static-files.s3.amazonaws.com/premierleague/photos/players/250x250/Photo-Missing.png"
+    
+    constructor(props) {
+        super(props);
+        const { player,team } = this.props;
+        this.state = {
+            selected: null,
+            player,
+            team,
+            errored: false
+        };
+    }
+
+    componentDidMount(){
+        const {player} = this.state
+        const ratingRef = firebase.database().ref(`players/${player.key}/ratings`);
+        const userRatingRef = firebase.database().ref(`userRatings/${this.props.uid}/${player.key}/`)
+        
+        ratingRef.on('child_changed',(snap) =>{
+            this.setState(prevState => {
+                const {player} = prevState;
+                const ratings = Object.assign({},player.ratings);
+                
+                ratings[snap.key] = snap.val();
+                player.ratings = ratings
+                
+                return {player};
+            });
+        })
+        userRatingRef.once('value',(snap) =>{
+            this.setState(prevState => ({
+                selected:snap.val()
+            }));
+        })
+        twemoji.parse(document.body);
+    }
+
+    render() {
+        const { player, selected, team } = this.state;
+        return (
+            <div className="player-card card">
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-left">
+                            <figure className="image is-96x96">
+                                { this.state.errored ? 
+                                   <img  src={this.missingImage} alt=""/>
+                                    :
+                                    <img onError={() =>this.handleError()} src={player.photo} alt=""/>
+                                }
+                            </figure>
+                        </div>
+                        <div className="media-content">
+                            <p className="title is-5">{player.firstName} {player.lastName}</p>
+                            <p className="subtitle is-6">{player.position}</p>
+                        </div>
+                    </div>
+                </div>
+                <footer className="card-footer">
+                    { this.emojis.map((emoji)=>{
+                    return (
+                        <button key={emoji.id} className={"button card-footer-item  is-large fx-col fx-j-c emoji-btn "+
+                        (selected === emoji.id ? 'is-active is-primary':'')}
+                        onClick={ () =>this.handleClick(emoji.id)}
+                        >
+                            <span className="icon u-margin-0"><i>{emoji.value}</i></span>
+                            {this.renderCount(selected,player.ratings[emoji.id] || 0)}
+                        </button>
+                    )
+                    }) 
+                }
+                </footer>
+            </div>
+        );
+    }
+
+    handleClick(id){  
+        this.setState(prevState => {
+            const {player,selected} = prevState;
+            const ratings = Object.assign({},player.ratings);
+            
+            ratings[id]++;
+            if(ratings[selected] > 0){
+                ratings[selected]--;
+            }
+            
+            player.ratings = ratings
+            var updates = {
+                new:id,
+            };
+            var refs ={
+                player:`/players/${player.key}/ratings`,
+                user:`/userRatings/${this.props.uid}`
+            }
+
+            if(selected){
+                updates.old = selected;
+            }
+            
+            this.updateRating(player,refs,updates);
+            
+            return {selected: id, player};
+        });
+    }
+
+    updateRating(player,refs,updates){
+       
+        firebase.database().ref(refs.player).transaction(ratings =>{
+            if(ratings){
+                ratings[updates.new]++;
+                if(updates.old){
+                    ratings[updates.old]--;
+                    ratings[updates.old] = ratings[updates.old] < 0 ? 0 : ratings[updates.old] ;
+                }   
+            }
+
+            return ratings;
+        });
+
+        var u = {}
+        u[player.key] = updates.new;
+        firebase.database().ref(refs.user).update(u);
+    }
+
+    renderCount(selected,amount = 0){
+        return (selected) ?
+            <span className="tag is-small vote-count">{amount}</span>
+            :
+            <span></span>
+    }
+
+    handleError() {
+        this.setState(prevState => {
+            return {errored: true}
+        });
+    }
+}
