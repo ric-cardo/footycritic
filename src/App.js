86942126b596add4bc0f8d8a5e1a052d12411ac8
Riccardo <riccardo@Riccardos-MacBook-Pro.local>
2017-05-11 07:36:56 +0100

    feat(app): add nav bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
+import twemoji from 'twemoji'; 
+
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      teams:[],
+      selected: 'null',
+    };
+  }
+
+  componentDidMount(){
+    const rootRef = firebase.database().ref();
+    const teamRef = rootRef.child('teams');
+
+    teamRef.on('value',snap =>{
+      const teamsObj = snap.val();
+      const teams =  Object.keys(teamsObj)
+        .map(key =>{
+          return Object.assign({key},teamsObj[key])
+        })
+        .sort((a,b) => a.name.localeCompare(b.name))
+        
+      this.setState(prevState => ({teams}));
+    })
+
+  }
+  selectTeam(name){
+    this.setState(prevState => ({
+      selected: name
+    }));
+  }
+
   render() {
+    const {selected,teams} = this.state
+    const team = teams.filter(team => team.name === selected)[0];
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="fx-col">
+        <div className="tabs is-toggle tabs-list-container">
+          <ul className="tabs-list">
+            { this.state.teams.map((team)=>{
+              return (
+                <li key={team.key} 
+                  className={(selected === team.name ? 'is-active':'')}
+                  onClick={() =>this.selectTeam(team.name)}>
+                  <a>
+                    <figure className="image is-32x32 fx-row fx-a-c fx-j-c">
+                      <img src={team.badge} alt=""/>
+                    </figure>
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as firebase from 'firebase';
+
+import 'bulma/css/bulma.css';
 import './index.css';
+
+const config = {
+  apiKey: "AIzaSyCWDYZaDPDGukKDFJ6ypTIMn3feSoplh0c",
+  authDomain: "footycritic-50b01.firebaseapp.com",
+  databaseURL: "https://footycritic-50b01.firebaseio.com",
+  projectId: "footycritic-50b01",
+  storageBucket: "footycritic-50b01.appspot.com",
+  messagingSenderId: "378932444128"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
